4108297ab1ce1bd2cb5551d82ef2dd756e4c2fc6
Maddy Guthridge <hello@maddyguthridge.com>
2025-02-14 23:24:41 +1100

    Add better CI

---
+++ tests/simple_test.py
@@ -0,0 +1,11 @@
+"""
+simple_test.py
+
+Very simple test cases.
+"""
+
+
+def test_it():
+    # Not a real test case, but I need one or CI will fail
+    # I'll add actual tests later
+    assert 1 + 1 == 2
